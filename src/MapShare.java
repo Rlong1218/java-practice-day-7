f325c67ce55b2fe62f4dce388972821907c8571b
Rlong1218 <7dbrobert1218@gmail.com>
2020-08-09 21:05:53 -0600

    map practice

---
+++ src/MapShare.java
@@ -0,0 +1,15 @@
+import java.util.Map;
+
+// Modify and return the given map as follows:
+// if the key "a" has a value, set the key "b" to have that same value.
+// In all cases remove the key "c", leaving the rest of the map unchanged.
+
+public class MapShare {
+    public static Map<String, String> mapShare(Map<String, String> map) {
+        if (map.containsKey("a")) {
+            map.put("b", map.get("a"));
+        }
+        map.remove("c");
+        return map;
+    }
+}
